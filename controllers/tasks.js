d97c87215e4db35b693eeea7da842a617d731814
stephtheitsloth <stephan.karim.sk@gmail.com>
2023-09-24 06:00:01 -0400

    controllers file bug fixed

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -59,7 +59,7 @@ const deleteSingleTask = async (req,res) => {
         const task = await Task.findOneAndDelete({_id: taskID})
 
         if(!task){
-            return res.status(404).json({ msg: `No task with id: $(taskID)`})
+            return res.status(404).json({ msg: `No task with id: ${taskID}`})
         }
 
         res.status(200).json({task})
